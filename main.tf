2e2bc007bf2699b5653461a384a2d693a5f7c240
lmgsaenz <lmgsaenz>
2023-09-07 17:39:55 +0200

    add checks to verify if you have to try to create subnets or not

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 // VPC
+locals {
+  length_public_subnets   = length(var.public_subnets)
+  length_private_subnets  = length(var.private_subnets)
+  length_database_subnets = length(var.database_subnets)
+}
 resource "aws_vpc" "this" {
   cidr_block           = var.cidr
   instance_tenancy     = var.instance_tenancy
@@ -13,8 +18,12 @@ resource "aws_vpc" "this" {
 }
 
 // Public Subnets
+locals {
+  create_public_subnets = local.length_public_subnets > 0
+}
+
 resource "aws_subnet" "public" {
-  count                   = length(var.public_subnets)
+  count                   = local.create_public_subnets ? local.length_public_subnets : 0
   vpc_id                  = aws_vpc.this.id
   cidr_block              = element(var.public_subnets, count.index)
   map_public_ip_on_launch = var.map_public_ip_on_launch
@@ -28,8 +37,12 @@ resource "aws_subnet" "public" {
 }
 
 // Private Subnets
+locals {
+  create_private_subnets = local.length_private_subnets > 0
+}
+
 resource "aws_subnet" "private" {
-  count      = length(var.private_subnets)
+  count      = local.create_private_subnets ? local.length_private_subnets : 0
   vpc_id     = aws_vpc.this.id
   cidr_block = element(var.private_subnets, count.index)
   tags = merge(
@@ -42,8 +55,12 @@ resource "aws_subnet" "private" {
 }
 
 // Database Subnets
+locals {
+  create_database_subnets = local.length_database_subnets > 0
+}
+
 resource "aws_subnet" "database" {
-  count      = length(var.database_subnets)
+  count      = local.create_database_subnets ? local.length_database_subnets : 0
   vpc_id     = aws_vpc.this.id
   cidr_block = element(var.database_subnets, count.index)
   tags = merge(

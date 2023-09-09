acf7e742a925916699702beba2cf5dc9a8e68395
lmgsaenz <lmgsaenz>
2023-09-09 10:09:14 +0200

    Add Name tag in the igw

--- main.tf
+++ main.tf
@@ -124,4 +124,5 @@ resource "aws_route_table_association" "database" {
 resource "aws_internet_gateway" "this" {
   count  = local.create_public_subnets && var.create_igw ? 1 : 0
   vpc_id = aws_vpc.this.id
+  tags   = { Name = "${var.name}-igw" }
 }
